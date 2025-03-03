496927ff1aa6345219be34ea454f5eaa73d906de
ztoswim <zerooo.to.ooone@gmail.com>
2025-03-03 15:09:36 +0800

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,21 +1,16 @@
-// db.js
-const mysql = require('mysql2');
+const mongoose = require("mongoose");
 
-// 创建连接对象
-const connection = mysql.createConnection({
-  host: 'localhost',     // 数据库服务器地址，通常是 localhost
-  user: 'root', // 数据库用户名（例如 root 或你创建的其他用户）
-  password: 'Perfect247965', // 数据库密码
-  database: 'mydb'  // 你要连接的数据库名称，比如 mydb
-});
-
-// 连接数据库
-connection.connect((err) => {
-  if (err) {
-    console.error("数据库连接失败", err);
-  } else {
-    console.log("数据库连接成功");
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
   }
-});
+};
 
-module.exports = connection;
+module.exports = connectDB;
