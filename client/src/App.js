140ee4678440250c92fcec82e73aa8271f9ee3de
ztoswim <zerooo.to.ooone@gmail.com>
2025-03-05 18:10:16 +0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import BossDashboard from "./pages/BossDashboard";
