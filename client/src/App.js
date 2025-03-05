e84777f8c253d4696d2462ffbe972f91ced75d17
ztoswim <zerooo.to.ooone@gmail.com>
2025-03-06 00:31:32 +0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { login } from "../api/authApi";
+import { login } from "api/authApi";
 import { FaUser, FaLock } from "react-icons/fa";
 import logo from "../assets/Logo.png";
 import "../styles/Login.css";
