a6992f1ff9349705ad334be9af4f7df05d08c91d
ztoswim <zerooo.to.ooone@gmail.com>
2025-03-06 11:26:08 +0800

    Update DashboardLayout.js

--- client/src/components/DashboardLayout.js
+++ client/src/components/DashboardLayout.js
@@ -1,14 +1,29 @@
-import SidebarNavbar from "./SidebarNavbar";
-import { Outlet } from "react-router-dom";
-import "../styles/DashboardLayout.css";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import Sidebar from "./Sidebar";
+import logo from "../assets/Logo.png";
 
-const DashboardLayout = () => {
+const Navbar = ({ toggleSidebar }) => {
   return (
-    <div className="dashboard-layout">
-      <SidebarNavbar />
-      <main className="dashboard-content">
-        <Outlet />
-      </main>
+    <nav className="lg:hidden fixed top-0 left-0 w-full bg-gray-900 text-white flex items-center justify-between p-4 z-50">
+      <img src={logo} alt="Logo" className="h-8" />
+      <button onClick={toggleSidebar} className="text-white text-2xl">
+        <FaBars />
+      </button>
+    </nav>
+  );
+};
+
+const DashboardLayout = ({ children }) => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  return (
+    <div className="flex h-screen">
+      <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
+      <div className="flex-1 flex flex-col">
+        <Navbar toggleSidebar={() => setIsCollapsed(!isCollapsed)} />
+        <main className="flex-1 p-4 mt-[3rem] lg:mt-0">{children}</main>
+      </div>
     </div>
   );
 };
