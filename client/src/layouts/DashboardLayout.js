596b154bdb8121358874e42d59462246a8f59591
ztoswim <zerooo.to.ooone@gmail.com>
2025-03-06 09:05:59 +0800

    Update DashboardLayout.js

--- client/src/layouts/DashboardLayout.js
+++ client/src/layouts/DashboardLayout.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
-import Sidebar from "./Sidebar";
-import Navbar from "./Navbar";
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
 import "../styles/DashboardLayout.css";
 
 const DashboardLayout = () => {
