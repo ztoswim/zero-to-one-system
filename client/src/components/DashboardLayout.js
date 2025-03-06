8df47a62d02894cff1971b212c46ba59ff2a2305
ztoswim <zerooo.to.ooone@gmail.com>
2025-03-06 13:53:07 +0800

    update

--- client/src/components/DashboardLayout.js
+++ client/src/components/DashboardLayout.js
@@ -1,18 +1,13 @@
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
-import "../styles/DashboardLayout.css";
 
 const DashboardLayout = ({ children }) => {
   return (
-    <div className="dashboard-container">
-      {/* 仅在大屏显示 Sidebar */}
-      <Sidebar className="lg:block hidden" />
-
-      <div className="dashboard-content">
-        {/* 仅在小屏显示 Navbar */}
-        <Navbar className="lg:hidden block" />
-
-        <main className="main-content">{children}</main>
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex flex-col flex-grow">
+        <Navbar />
+        <main className="flex-grow p-4 overflow-auto">{children}</main>
       </div>
     </div>
   );
