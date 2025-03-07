243f8d01862ea3cb03081546c83a88b8f405f45b
ztoswim <zerooo.to.ooone@gmail.com>
2025-03-07 08:28:12 +0800

    Update vite.config.js

--- client/src/vite.config.js
+++ client/src/vite.config.js
@@ -4,9 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // 确保这里是 5173
-    open: true, // 启动后自动打开浏览器
+    port: 5173,
+    open: true,
   },
+  publicDir: "public", // 确保 Vite 使用 public/index.html
   resolve: {
     alias: {
       "@": "/src",
