17046c2303c96c60e63c0d945f27d54edf7e85e9
ztoswim <zerooo.to.ooone@gmail.com>
2025-03-07 08:25:24 +0800

    update

--- client/src/vite.config.js
+++ client/src/vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // 确保这里是 5173
+    open: true, // 启动后自动打开浏览器
+  },
   resolve: {
     alias: {
       "@": "/src",
