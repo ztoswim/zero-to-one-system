4a8f1cf85b865aed19fbe52ef4ee476b032f32c4
ztoswim <zerooo.to.ooone@gmail.com>
2025-03-07 09:36:06 +0800

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // 插件配置：集成 React 插件
   plugins: [react()],
-
-  // 开发服务器配置
-  server: {
-    port: 5173, // 设置开发服务器端口
-    open: true, // 启动时自动打开浏览器
-  },
-
-  // 解析配置，设置路径别名
-  resolve: {
-    alias: {
-      "@": "/src", // 将 "@" 别名指向 "src" 目录，简化导入路径
+  esbuild: {
+    loader: {
+      '.js': 'jsx',  // 设置 .js 文件使用 JSX 语法
     },
   },
+  server: {
+    open: true, // 启动时自动打开浏览器
+  }
 });
