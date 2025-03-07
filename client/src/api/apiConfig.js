4c934d6ce663fc81cc78294ff22f46d6d7c509cb
ztoswim <zerooo.to.ooone@gmail.com>
2025-03-07 15:04:11 +0800

    delete

--- client/src/api/apiConfig.js
+++
@@ -1,7 +0,0 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
-if (!API_BASE_URL) {
-  console.error("API_BASE_URL is not defined. Please check your .env file.");
-}
-
-export default API_BASE_URL;
