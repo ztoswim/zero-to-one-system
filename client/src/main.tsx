c62c7cb29af67df72757a0ec398e90246420aeba
ztoswim <zerooo.to.ooone@gmail.com>
2025-03-08 22:29:13 +0800

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { useState, SetStateAction } from 'react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App setUserRole={setUserRole} />
   </StrictMode>,
 )
+function setUserRole(value: SetStateAction<string | null>): void {
+  const [, setUserRoleState] = useState<string | null>(null);
+  setUserRoleState(value);
+}
+
