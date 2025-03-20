e6cbb96f5b3fd340a298665a5920b7b4b330a8ab
ztoswim <zerooo.to.ooone@gmail.com>
2025-03-20 13:56:27 +0800

    Update DashboardLayout.tsx

--- client/src/components/DashboardLayout.tsx
+++ client/src/components/DashboardLayout.tsx
@@ -20,19 +20,21 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   if (!role) return null;
 
   return (
-    <div className="flex bg-gray-100">
-      {/* Sidebar 默认隐藏，鼠标指向左边缘展开 */}
-      <Sidebar role={role} />
+    <div className="flex bg-gray-100 min-h-screen">
+      {/* 只在大屏幕显示 Sidebar */}
+      <div className="hidden md:flex">
+        <Sidebar role={role} />
+      </div>
 
       {/* 右侧内容区 */}
-      <div className="flex-1 flex flex-col transition-all duration-300 ml-0">
-        {/* 小屏幕显示 Navbar */}
-        <div className="md:hidden w-full top-0 left-0 bg-gray-800 z-20 fixed">
+      <div className="flex-1 flex flex-col">
+        {/* 小屏幕显示 Navbar，但不遮挡内容 */}
+        <div className="md:hidden w-full bg-gray-800 z-20 sticky top-0">
           <Navbar role={role} />
         </div>
 
-        {/* 页面内容 */}
-        <div className="flex-1 p-4 mt-16 md:mt-0">
+        {/* 页面内容，顶部留出空间以避免 Navbar 遮挡 */}
+        <div className="flex-1 p-4 pt-16 md:pt-4">
           {children}
         </div>
       </div>
